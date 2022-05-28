d6093615d274f4cf21df15fd8589a36953cc189e
Ariana Maria <ariana.moschopoulou@gmail.com>
2022-05-28 20:23:48 +0100

    added dropdown menu to select number of users

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const randomFolks = document.querySelector(".random-peeps");
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const selectUserNumber = document.querySelector("#users");
+const dropdown = document.querySelector(".num-users")
+
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
+    
     const data = await usersRequest.json();
-    //console.log(data);
+
     const userResults = data.results;
-    //console.log(userResults);
     displayUsers(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
     randomFolks.innerHTML = "";
@@ -24,3 +27,8 @@ const displayUsers = function (userResults) {
     }
 };
 
+dropdown.classList.remove("hide");
+selectUserNumber.addEventListener("change", function (e){
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
